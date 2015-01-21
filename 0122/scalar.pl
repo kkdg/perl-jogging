702a4a1096db2e678635ef7dba25dd01c249e3a2
Kwag De Gi <code@skyaperture.com>
2015-01-22 00:18:43 +0900

    scalar type

---
+++ 0122/scalar.pl
@@ -0,0 +1,13 @@
+
+
+$smile = v9786;
+$foo = v102.111.111;
+$martin = v77.97.114.116.105.110;
+
+print $smile . "\n";
+print $foo . "\n";
+print $martin . "\n";
+
+print __FILE__ . "\n";
+print __LINE__ . "\n";
+print __PACKAGE__ . "\n";

e882205bc0a30774f4b1deb68049a17ba12c9d11
Kwag De Gi <code@skyaperture.com>
2015-02-02 07:37:32 +0900

    sub returns implicitly the last return value of calculation even without explicit declare

--- 0202/sub.pl
+++ 0202/sub.pl
@@ -10,3 +10,19 @@ sub marine {
 
 &marine;
 print $_;
+
+
+sub sum_of_em {
+	print "hi, this again good! \n";
+	$fred + $barney;
+}
+
+$fred = 1;
+$barney = 2;
+
+$wilma = &sum_of_em;
+print $wilma;
+
+
+
+print "\n";
